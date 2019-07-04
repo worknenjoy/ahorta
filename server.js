b77a56018ee852119b7c43549aa1c5440538c1cf
Alexandre Magno <alexanmtz@gmail.com>
2019-07-05 00:21:06 +0200

    enabling cors for localhost

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const Notify = require('./mail')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
-  app.use(cors())
 }
+
+app.use(cors())
+app.options('localhost:3000', cors());
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
