64dda2924215fccc9be48480e00c3b612f32dfa3
Alexandre Magno <alexanmtz@gmail.com>
2019-10-01 20:48:45 +0200

    adjusting to model associations using plural

--- server.js
+++ server.js
@@ -205,8 +205,8 @@ app.post('/sensor', async (req, res) => {
       })
       if(device) {
         if(humidity) {
-          console.log('device readings', device.Reading)
-          if(device.Reading && device.Reading.length && humidity === device.Reading[0].value) return res.status(200).json({timer: device.timer}).end()
+          console.log('device readings', device.Readings)
+          if(device.Readings && device.Readings.length && humidity === device.Readings[0].value) return res.status(200).json({timer: device.timer}).end()
           Notify.sensor(device.email, humidity)
           const userReading = await device.createReading({value: humidity})
           //return res.status(200).json({user, ...{reading: userReading}}).end()
