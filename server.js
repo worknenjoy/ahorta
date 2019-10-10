31a7bc6ae0a6b370a9fa72d7a71c76d01264d651
Alexandre Magno <alexanmtz@gmail.com>
2019-10-10 09:36:10 +0200

    just notify with percent less than 40

--- server.js
+++ server.js
@@ -207,7 +207,8 @@ app.post('/sensor', async (req, res) => {
         if(humidity) {
           console.log('device readings last with humidity', device.Readings[0], humidity)
           if(device.Readings && device.Readings.length && parseInt(humidity) === parseInt(device.Readings[0].dataValues.value)) return res.status(200).json({timer: device.timer}).end()
-          Notify.sensor(device.email, humidity)
+          const percent = Math.round(humidity * 100 / 1024)
+          if(percent < 40) Notify.sensor(device.email, humidity)
           const userReading = await device.createReading({value: humidity})
           //return res.status(200).json({user, ...{reading: userReading}}).end()
           return res.status(200).json({timer: device.timer}).end()
